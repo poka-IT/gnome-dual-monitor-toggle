90e7b7c352a5a265149ea87a3463f7e582538ebf
poka <poka@p2p.legal>
2024-04-09 18:51:37 +0200

    apply @JustPerfect review

--- extension.js
+++ extension.js
@@ -1,66 +1,93 @@
 import GLib from 'gi://GLib';  
 import GObject from 'gi://GObject';
-import {QuickToggle, QuickMenuToggle, SystemIndicator} from 'resource:///org/gnome/shell/ui/quickSettings.js';
+import Gio from 'gi://Gio';
+import {QuickMenuToggle, SystemIndicator} from 'resource:///org/gnome/shell/ui/quickSettings.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
 const SecondMonitorToggle = GObject.registerClass(
 class SecondMonitorToggle extends QuickMenuToggle {
     _init(indicator) {
-        log(`Initializing Dual Monitor Toggle`);
+        console.debug(`Initializing Dual Monitor Toggle`);
+
         super._init({
             title: _('Second Monitor'),
             iconName: 'video-display-symbolic',
             toggleMode: true,
         });
 
+        // Add callback to communicate_utf8_async method
+        Gio._promisify(Gio.Subprocess.prototype, 'communicate_utf8_async');
+
+        // Set initial values for monitor configuration
         this._indicator = indicator;
         this._mode = '1920x1080';
         this._pos = '2560x199';
         this._monitor = 'HDMI-0';
+        this._timeoutId = null;
 
-        this._getAvailableMonitors();
-        this._buildMonitorMenu();
-        
-        GLib.timeout_add(GLib.PRIORITY_DEFAULT, 100, () => {
-            this._getMonitorConfig();
-            this._sync();
+        // Fetch available monitors and build monitor menu
+        this._getAvailableMonitors().then(() => {
+            this._buildMonitorMenu();
+            this._timeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 100, () => {
+                this._getMonitorConfig();
+                this._sync();
+                return GLib.SOURCE_CONTINUE;
+            });
         });
+
         this.connect('clicked', this._toggleSecondMonitor.bind(this));
     }
     
-    _getAvailableMonitors() {
+    async _getAvailableMonitors() {
         this._monitors = [];
         try {
-            let [ok, output, err, exit] = GLib.spawn_command_line_sync('xrandr --query');
-            if (ok) {
-                let lines = output.toString().split('\n');
-                for (let line of lines) {
-                    if (line.includes(' connected ')) {
-                        let monitor = line.split(' ')[0];
-                        this._monitors.push(monitor);
-                    }
+            // Run xrandr command to query monitor information
+            let proc = Gio.Subprocess.new(['xrandr', '--query'], Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE);
+            let [stdout, stderr] = await proc.communicate_utf8_async(null, null);
+            if (!proc.get_successful()) {
+                throw new Error(stderr);
+            }
+            // Extract monitor names from xrandr output
+            let lines = stdout.split('\n');
+            for (let line of lines) {
+                if (line.includes(' connected ')) {
+                    let monitor = line.split(' ')[0];
+                    this._monitors.push(monitor);
                 }
             }
         } catch (e) {
-            logError(e);
+            console.error(e);
         }
     }
 
     _buildMonitorMenu() {
         this.menu.removeAll();
-        
+
+        // Add the menu title
+        let menuTitle = new PopupMenu.PopupImageMenuItem(_("Select a monitor"), 'video-display-symbolic', {
+            reactive: false,
+            style_class: 'selectLabel'
+        });
+        menuTitle._icon.icon_size = 24;
+        this.menu.addMenuItem(menuTitle);
+
+        // Add a separator
+        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+
+        // Add menu items for each available monitor
         for (let monitor of this._monitors) {
             let item = new PopupMenu.PopupMenuItem(monitor);
             item.connect('activate', () => {
-                this._monitor = monitor;
-                this._getMonitorConfig();
-                this._sync();
+            this._monitor = monitor;
+            this._getMonitorConfig();
+            this._sync();
             });
             this.menu.addMenuItem(item);
         }
     }
-    
+
+    // Sync the toggle state with the number of connected monitors
     _sync() {
         const nMonitors = global.display.get_n_monitors();
         this.checked = nMonitors > 1;
@@ -75,40 +102,62 @@ class SecondMonitorToggle extends QuickMenuToggle {
         }
     }
     
-    _getMonitorConfig() {
+    async _getMonitorConfig() {
         try {
-            let [ok, output, err, exit] = GLib.spawn_command_line_sync(`xrandr --query | grep "${this._monitor} connected"`);
-            if (ok) {
-                let line = output.toString();
-                let modeMatch = line.match(/(\d+x\d+\+\d+\+\d+)/);
-                if (modeMatch) {
-                    let modeStr = modeMatch[1];
-                    let [mode, ...pos] = modeStr.split('+');
-                    this._mode = mode;
-                    this._pos = pos.join('x');
+            // Run xrandr command to query monitor information
+            let proc = Gio.Subprocess.new(['xrandr', '--query'], Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE);
+            let [stdout, stderr] = await proc.communicate_utf8_async(null, null);
+            if (!proc.get_successful()) {
+                throw new Error(stderr);
+            }
+            // Extract monitor configuration from xrandr output
+            let lines = stdout.split('\n');
+            for (let line of lines) {
+                if (line.includes(`${this._monitor} connected`)) {
+                    let modeMatch = line.match(/(\d+x\d+\+\d+\+\d+)/);
+                    if (modeMatch) {
+                        let modeStr = modeMatch[1];
+                        let [mode, ...pos] = modeStr.split('+');
+                        this._mode = mode;
+                        this._pos = pos.join('x');
+                    }
+                    break;
                 }
             }
         } catch (e) {
-            logError(e);
+            console.error(e);
         }
     }
     
-    _toggleSecondMonitor() {
+    async _toggleSecondMonitor() {
         this._sync();
         if (this.checked) {
-            log(`Disabling second monitor`);
-            GLib.spawn_command_line_async(`xrandr --output ${this._monitor} --off`);
+            console.debug(`Disabling second monitor`);
+            // Disable the second monitor using xrandr command
+            Gio.Subprocess.new(['xrandr', '--output', this._monitor, '--off'], Gio.SubprocessFlags.NONE);
         } else {
-            log(`Enabling second monitor`);
+            console.debug(`Enabling second monitor`);
             if (this._mode && this._pos) {
-                GLib.spawn_command_line_async(`xrandr --output ${this._monitor} --mode ${this._mode} --pos ${this._pos}`);
+                // Enable the second monitor using xrandr command
+                Gio.Subprocess.new(['xrandr', '--output', this._monitor, '--mode', this._mode, '--pos', this._pos], Gio.SubprocessFlags.NONE);
             } else {
-                log('Failed to get monitor configuration');
+                console.warn('Failed to get monitor configuration');
             }
         }
+        
         GLib.timeout_add(GLib.PRIORITY_DEFAULT, 1000, () => {
             this._sync();
+            return GLib.SOURCE_REMOVE;
         });
+    }
+    
+    destroy() {
+        if (this._timeoutId) {
+            GLib.Source.remove(this._timeoutId);
+            this._timeoutId = null;
+        }
+        
+        super.destroy();
     }
 });
 
@@ -124,14 +173,14 @@ class SecondMonitorIndicator extends SystemIndicator {
     }
 });
 
-export default class Extension {
+export default class DualMonitorExtension {
     enable() {
         this._indicator = new SecondMonitorIndicator();
         Main.panel.statusArea.quickSettings.addExternalIndicator(this._indicator);
     }
     
     disable() {
-        log(`Disabling Dual Monitor Toggle`);
+        console.debug(`Disabling Dual Monitor Toggle`);
         
         this._indicator.quickSettingsItems.forEach(item => item.destroy());
         this._indicator.quickSettingsItems = [];
