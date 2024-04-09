2e4ebcd8582900d93f113f8f575a932f1604d3d1
poka <poka@p2p.legal>
2024-04-09 21:53:39 +0200

    refac monitor enabling

--- extension.js
+++ extension.js
@@ -21,51 +21,23 @@ class SecondMonitorToggle extends QuickMenuToggle {
 
         // Set initial values for monitor configuration
         this._indicator = indicator;
-        this._mode = '1920x1080';
-        this._pos = '2560x199';
-        this._monitor = 'HDMI-0';
-        this._timeoutId = null;
+        this._configsMap = {};
 
         // Fetch available monitors and build monitor menu
-        this._getAvailableMonitors().then(() => {
+        this._getMonitorConfig().then(() => {
             this._buildMonitorMenu();
-            this._timeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 100, () => {
-                this._getMonitorConfig();
-                this._sync();
-                return GLib.SOURCE_CONTINUE;
-            });
+            this._updateSelectedMonitor();
+            this._sync();
         });
 
         this.connect('clicked', this._toggleSecondMonitor.bind(this));
-    }
-    
-    async _getAvailableMonitors() {
-        this._monitors = [];
-        try {
-            // Run xrandr command to query monitor information
-            let proc = Gio.Subprocess.new(['xrandr', '--query'], Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE);
-            let [stdout, stderr] = await proc.communicate_utf8_async(null, null);
-            if (!proc.get_successful()) {
-                throw new Error(stderr);
-            }
-            // Extract monitor names from xrandr output
-            let lines = stdout.split('\n');
-            for (let line of lines) {
-                if (line.includes(' connected ')) {
-                    let monitor = line.split(' ')[0];
-                    this._monitors.push(monitor);
-                }
-            }
-        } catch (e) {
-            console.error(e);
-        }
     }
 
     _buildMonitorMenu() {
         this.menu.removeAll();
 
         // Add the menu title
-        let menuTitle = new PopupMenu.PopupImageMenuItem(_("Select a monitor"), 'video-display-symbolic', {
+        const menuTitle = new PopupMenu.PopupImageMenuItem(_("Select a monitor"), 'video-display-symbolic', {
             reactive: false,
             style_class: 'selectLabel'
         });
@@ -76,10 +48,10 @@ class SecondMonitorToggle extends QuickMenuToggle {
         this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
         // Add menu items for each available monitor
-        for (let monitor of this._monitors) {
-            let item = new PopupMenu.PopupMenuItem(monitor);
+        for (const [monitorName, _] of Object.entries(this._configsMap)) {
+            const item = new PopupMenu.PopupMenuItem(monitorName);
             item.connect('activate', () => {
-                this._monitor = monitor;
+                this._monitor = monitorName;
                 this._getMonitorConfig();
                 this._sync();
             });
@@ -96,7 +68,7 @@ class SecondMonitorToggle extends QuickMenuToggle {
 
     // Update the selected monitor visual in the menu
     _updateSelectedMonitor() {
-        for (let item of this.menu._getMenuItems()) {
+        for (const item of this.menu._getMenuItems()) {
             if (item.label.text === this._monitor) {
                 item.add_style_class_name('selectedMonitor');
             } else {
@@ -112,28 +84,41 @@ class SecondMonitorToggle extends QuickMenuToggle {
             this._indicator.visible = false;
         }
     }
-    
+
     async _getMonitorConfig() {
         try {
             // Run xrandr command to query monitor information
-            let proc = Gio.Subprocess.new(['xrandr', '--query'], Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE);
-            let [stdout, stderr] = await proc.communicate_utf8_async(null, null);
+            const proc = Gio.Subprocess.new(['xrandr', '--query'], Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE);
+            const [stdout, stderr] = await proc.communicate_utf8_async(null, null);
             if (!proc.get_successful()) {
                 throw new Error(stderr);
             }
-            // Extract monitor configuration from xrandr output
-            let lines = stdout.split('\n');
-            for (let line of lines) {
-                if (line.includes(`${this._monitor} connected`)) {
-                    let modeMatch = line.match(/(\d+x\d+\+\d+\+\d+)/);
-                    if (modeMatch) {
-                        let modeStr = modeMatch[1];
+            const isConfigsMapEmpty = Object.keys(this._configsMap).length === 0;
+            const lines = stdout.split('\n');
+            for (const line of lines) {
+                // Extract enabled monitor names from xrandr output
+                const modeMatch = line.match(/(\d+x\d+\+\d+\+\d+)/); 
+                if (line.includes(' connected ') && modeMatch) {
+                    const monitor = line.split(' ')[0];
+                    if (isConfigsMapEmpty) {
+                        const modeStr = modeMatch[1];
                         let [mode, ...pos] = modeStr.split('+');
-                        this._mode = mode;
-                        this._pos = pos.join('x');
+                        pos = pos.join('x');
+                        this._configsMap[monitor] = {mode, pos};
                     }
-                    break;
+                    // Set current monitor control
+                    if (!line.includes(' primary ') && !this._monitor) {
+                        this._monitor = monitor;
+                    }
                 }
+            }
+            if (isConfigsMapEmpty) {
+                // Sort configsMap by order of pos num value (lower first)
+                this._configsMap = new Map([...this._configsMap.entries()].sort((a, b) => {
+                    const posA = parseInt(a[1].pos.split('x')[0]);
+                    const posB = parseInt(b[1].pos.split('x')[0]);
+                    return posA - posB;
+                }));
             }
             this._updateSelectedMonitor();
         } catch (e) {
@@ -144,20 +129,24 @@ class SecondMonitorToggle extends QuickMenuToggle {
     async _toggleSecondMonitor() {
         this._sync();
         if (this.checked) {
-            console.debug(`Disabling second monitor`);
-            // Disable the second monitor using xrandr command
+            console.debug(`Disabling monitor: ${this._monitor}`);
             Gio.Subprocess.new(['xrandr', '--output', this._monitor, '--off'], Gio.SubprocessFlags.NONE);
         } else {
-            console.debug(`Enabling second monitor`);
-            if (this._mode && this._pos) {
-                // Enable the second monitor using xrandr command
-                Gio.Subprocess.new(['xrandr', '--output', this._monitor, '--mode', this._mode, '--pos', this._pos], Gio.SubprocessFlags.NONE);
+            console.debug(`Enabling monitor: ${this._monitor}`);
+            if (Object.keys(this._configsMap).length > 0) {
+                // Build xrandr command to restore initial monitor configuration
+                let cmd = ['xrandr'];
+                Object.entries(this._configsMap).forEach(([monitor, config]) => {
+                    cmd.push('--output', monitor, '--mode', config.mode, '--pos', config.pos);
+                });
+                // Run xrandr command
+                Gio.Subprocess.new(cmd, Gio.SubprocessFlags.NONE);
             } else {
-                console.warn('Failed to get monitor configuration');
+                console.warn('Failed to restore initial monitor configuration');
             }
         }
         
-        GLib.timeout_add(GLib.PRIORITY_DEFAULT, 1000, () => {
+        this._timeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 1000, () => {
             this._sync();
             return GLib.SOURCE_REMOVE;
         });
@@ -168,7 +157,6 @@ class SecondMonitorToggle extends QuickMenuToggle {
             GLib.Source.remove(this._timeoutId);
             this._timeoutId = null;
         }
-        
         super.destroy();
     }
 });
